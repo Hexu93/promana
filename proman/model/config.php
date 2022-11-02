f6be93644caaab3a66c49f8827413fc466e91a44
Helena Holopainen <95844460+Hexu93@users.noreply.github.com>
2022-11-02 15:40:58 +0200

    Add files via upload

---
+++ proman/model/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Database connection configuration
+*/
+
+$host       = "mariadb.vamk.fi";
+$username   = "e2101194";
+$password   = "VSaCcubpEsm";
+$dbname     = "e2101194_proman";
+$options    = array(
+                PDO: :ATTR_ERRMODE => PDO: :ERRMODE_EXCEPTION
+                );
+
+?>

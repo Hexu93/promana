bd3fd04feacb55008e33c8b1debb5c2988d4176e
Helena Holopainen <e2101194@edu.vamk.fi>
2022-11-09 09:18:20 +0200

    nav and index created

---
+++ proman/index.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'theTrackerApp';
+
+ob_start();
+require 'views/nav.php';
+
+?>
+<div class="welcome">
+    <h1>Welcome to theTrackerApp</h1>
+    <p>an app that helps you track time you spend on various tasks</p>
+</div>
+<?php$content = ob_get_clean();
+include 'views/layout.php';
+?>
---
+++ proman/views/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/~e2101194/php/proman/promana/proman/">theTrackerApp</a></li>
+        <li><a href="controller/project_list.php">Project list</a></li>
+        <li><a href="controller/tasks_list.php">Tasks list </a></li>
+    </ul>
+</nav>
